13517ed1dc57a1cc69cc4b9b37f9b41571e6a30c
Seyfullah <seyfullahpolat@teknasyon.com>
2019-06-12 16:08:04 +0300

    [feat] Added custom line view supporting light and dark themes

---
+++ desk360/src/main/java/com/teknasyon/desk360/helper/Desk360LineView.kt
@@ -0,0 +1,24 @@
+package com.teknasyon.desk360.helper
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.widget.TextView
+import com.teknasyon.desk360.R
+import com.teknasyon.desk360.helper.Desk360Constants.currentTheme
+
+class Desk360LineView: TextView {
+    init {
+        if (currentTheme == "light") {
+            this.setBackgroundColor(Color.parseColor("#1F000000"))
+        } else {
+            this.setBackgroundColor(Color.parseColor("#1FA0A0A0"))
+        }
+    }
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+}
