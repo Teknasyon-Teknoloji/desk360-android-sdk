991a59c0e21309171a11e5941835d913517244f0
Seyfullah <seyfullahpolat@teknasyon.com>
2019-06-12 15:33:14 +0300

    [feat] Added new support message's api

--- desk360/src/main/java/com/teknasyon/desk360/connection/HttpService.kt
+++ desk360/src/main/java/com/teknasyon/desk360/connection/HttpService.kt
@@ -1,15 +1,12 @@
 package com.teknasyon.desk360.connection
 
-import com.teknasyon.desk360.model.MessageResponse
-import com.teknasyon.desk360.model.TickeMessage
-import com.teknasyon.desk360.model.TicketListResponse
-import com.teknasyon.desk360.model.TicketResponse
+import com.teknasyon.desk360.model.*
 import retrofit2.Call
 import retrofit2.http.*
 
 interface HttpService {
     @POST("api/v1/tickets")
-    fun addTicket(@Body ticketResponse: TicketResponse): Call<Any>
+    fun addTicket(@Body ticketItem: TicketReq): Call<NewSupportResponse>
 
     @GET("api/v1/tickets")
     fun getTicket(): Call<TicketListResponse>
@@ -21,5 +18,5 @@ interface HttpService {
     fun addMessage(@Path("ticket_id") ticket_id: Int, @Query("message") message: String): Call<MessageResponse>
 
     @GET("api/v1/tickets/types/list")
-    fun getTypeList(): Call<Any>
+    fun getTypeList(): Call<TypeResponse>
 }
