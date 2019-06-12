f62a9777f2df2bae98dc6cfd13d77945e83fc829
Seyfullah <seyfullahpolat@teknasyon.com>
2019-06-12 16:18:40 +0300

    [feat] Added custom BoldTextView supporting light and dark themes

---
+++ desk360/src/main/java/com/teknasyon/desk360/helper/TextViewBold.kt
@@ -0,0 +1,24 @@
+package com.smartalarm.sleeptic.helper
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.widget.TextView
+
+class TextViewBold : TextView {
+
+    constructor(context: Context) : super(context) {
+        val face = Typeface.createFromAsset(context.assets, "ProximaNova-Bold.otf")
+        this.typeface = face
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        val face = Typeface.createFromAsset(context.assets, "ProximaNova-Bold.otf")
+        this.typeface = face
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        val face = Typeface.createFromAsset(context.assets, "ProximaNova-Bold.otf")
+        this.typeface = face
+    }
+}
