9225ed88af61e11bd578310e0ea1e0d2d0b52b87
Seyfullah <seyfullahpolat@teknasyon.com>
2019-06-14 14:30:25 +0300

    [feat] Added sample Application class

---
+++ sample/src/main/java/com/teknasyon/desk360examp/Desk360App.kt
@@ -0,0 +1,13 @@
+package com.teknasyon.desk360examp
+
+import android.app.Application
+
+/**
+ * Created by seyfullah on 14,June,2019
+ *
+ */
+class  Desk360App: Application(){
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
