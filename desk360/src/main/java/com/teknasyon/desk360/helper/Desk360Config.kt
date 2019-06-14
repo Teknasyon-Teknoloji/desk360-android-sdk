b942f1b1070417cd4002a586b4f64c05ca003047
Seyfullah <seyfullahpolat@teknasyon.com>
2019-06-14 14:29:27 +0300

    [feat] Added Desk360Config class

---
+++ desk360/src/main/java/com/teknasyon/desk360/helper/Desk360Config.kt
@@ -0,0 +1,37 @@
+package com.teknasyon.desk360.helper
+
+import android.content.Context
+
+/**
+ * Created by seyfullah on 14,June,2019
+ *
+ */
+open class Desk360Config {
+    private lateinit var desk360Context: Context
+    var context: Context
+        get() {
+            return desk360Context
+        }
+        set(value) {
+            desk360Context = value
+            INSTANCE = this
+            desk360Preferences = Desk360Preferences()
+        }
+
+    fun getDesk360Preferences(): Desk360Preferences? {
+        return desk360Preferences
+    }
+
+    companion object {
+        var INSTANCE: Desk360Config? = null
+        private var desk360Preferences: Desk360Preferences? = null
+
+        val instance: Desk360Config
+            get() {
+                if (INSTANCE == null) {
+                    INSTANCE = Desk360Config()
+                }
+                return INSTANCE as Desk360Config
+            }
+    }
+}
