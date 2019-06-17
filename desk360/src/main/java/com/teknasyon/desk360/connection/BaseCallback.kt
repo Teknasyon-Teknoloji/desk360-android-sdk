6adf0ebbe6fcb2d4a652a2531f87558e4c5ead2e
Seyfullah <seyfullahpolat@teknasyon.com>
2019-06-17 14:40:27 +0300

    [fix] Fixed to null contol on Logger message

--- desk360/src/main/java/com/teknasyon/desk360/connection/BaseCallback.kt
+++ desk360/src/main/java/com/teknasyon/desk360/connection/BaseCallback.kt
@@ -9,14 +9,12 @@ abstract class BaseCallback<T> : Callback<T> {
 
     override fun onResponse(call: Call<T>, response: Response<T>) {
         onResponseSuccess(call, response)
-        Log.d("desk360success ->" + call.request().method().toString(), response.body()?.toString())
 
     }
 
     abstract fun onResponseSuccess(call: Call<T>, response: Response<T>)
 
     override fun onFailure(call: Call<T>, t: Throwable) {
-        Log.d("desk360error ->" + call.request().method().toString(), t.message?.let { it })
-//        t.message?.let { Logger.log("", it) }
+        t.message?.let { Log.e("", it) }
     }
 }
