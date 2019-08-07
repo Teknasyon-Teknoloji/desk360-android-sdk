1be6651c73819efb27af3e226cd429a4043b5c71
Seyfullah <seyfullahpolat@teknasyon.com>
2019-08-07 11:29:10 +0300

    [feat] Added Desk360Constants helper class

---
+++ desk360/src/main/java/com/teknasyon/desk360/helper/Desk360Constants.kt
@@ -0,0 +1,82 @@
+package com.teknasyon.desk360.helper
+
+import android.content.Context
+import android.os.Build
+import android.telephony.TelephonyManager
+import java.util.*
+
+/**
+ * Created by seyfullah on 09/04/2019.
+ */
+
+object Desk360Constants {
+
+    var currentTheme: String = "light"
+    var app_key: String? = null
+    var app_version: String? = null
+    var language_code: String? = null
+    var time_zone: String? = null
+    var baseURL: String? = null
+
+    fun desk360Config(
+        app_key_by_debug: String, device_token: String? = null, app_version: String?, baseURL: String
+    ): Boolean {
+
+        if (app_key_by_debug == "")
+            return false
+
+        if (app_version == "")
+            return false
+
+        if (language_code == "")
+            return false
+
+        if (time_zone == "")
+            return false
+
+        if (baseURL == "")
+            return false
+
+        if (device_token != null && device_token != "")
+            Desk360Config.instance.getDesk360Preferences()?.adId = device_token
+
+        this.app_key = app_key_by_debug
+        this.app_version = app_version
+        this.language_code = Locale.getDefault().language
+        this.time_zone = TimeZone.getDefault().id
+        this.baseURL = baseURL
+        return true
+    }
+
+    fun desk360CurrentTheme(current_theme: String) {
+        if (current_theme == "")
+            return
+        this.currentTheme = current_theme
+    }
+
+    fun countryCode(): String {
+
+        val tm = Desk360Config.instance.context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        return if (tm.networkCountryIso != null && tm.networkCountryIso != "") tm.networkCountryIso else Locale.getDefault().country
+    }
+
+    /**
+     * Generated a unique [deviceId]
+     *
+     */
+    fun getDeviceId() {
+        val devicesId = Desk360Config.instance.getDesk360Preferences()?.adId
+        if (devicesId != null && devicesId != "") {
+            return
+        }
+
+        val date = Date()
+        date.time
+
+        val deviceId = date.time.toString() + Build.VERSION.SDK_INT + "-" + Build.VERSION.INCREMENTAL + Build.MODEL
+
+        deviceId.let {
+            Desk360Config.instance.getDesk360Preferences()!!.adId = it
+        }
+    }
+}
