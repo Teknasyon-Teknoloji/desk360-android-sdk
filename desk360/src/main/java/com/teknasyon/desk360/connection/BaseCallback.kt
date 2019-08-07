eeb8d49899912b9541639fdfca24bd180f5e0bbc
Seyfullah <seyfullahpolat@teknasyon.com>
2019-08-07 11:20:36 +0300

    [feat] Added session control

--- desk360/src/main/java/com/teknasyon/desk360/connection/BaseCallback.kt
+++ desk360/src/main/java/com/teknasyon/desk360/connection/BaseCallback.kt
@@ -1,20 +1,95 @@
 package com.teknasyon.desk360.connection
 
 import android.util.Log
+import com.teknasyon.desk360.helper.Desk360Config
+import com.teknasyon.desk360.helper.Desk360Constants
+import com.teknasyon.desk360.helper.ResponseListener
+import com.teknasyon.desk360.model.Register
+import com.teknasyon.desk360.model.RegisterResponse
+import org.json.JSONException
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 abstract class BaseCallback<T> : Callback<T> {
 
-    override fun onResponse(call: Call<T>, response: Response<T>) {
-        onResponseSuccess(call, response)
+    private var cloneRequest: Call<T>? = null
+    private val call: Call<T>? = null
+    private val retryCount = 0
 
+    var listener: ResponseListener? = null
+    override fun onResponse(call: Call<T>, response: Response<T>) {
+        if (response.code() == 400) {
+            if (!response.isSuccessful) {
+
+                try {
+                    val jsonObject = JSONObject(response.errorBody()?.string())
+
+                    val errorCode: String =
+                        JSONObject(JSONObject(jsonObject.getString("meta")).getString("error")).getString("code")
+
+                    if (errorCode == "expired_at") {
+                        cloneRequest = call.clone()
+                        val register = Register()
+                        register.app_key = Desk360Constants.app_key
+                        register.device_id = Desk360Config.instance.getDesk360Preferences()?.adId
+                        register.app_platform = "Android"
+                        register.app_version = Desk360Constants.app_version
+                        register.language_code = Desk360Constants.language_code
+                        register.time_zone = Desk360Constants.time_zone
+
+                        Desk360RetrofitFactory.instance.sslService.register(register)
+                            .enqueue(object : Callback<RegisterResponse> {
+
+                                override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
+
+                                }
+
+                                override fun onResponse(
+                                    callRegister: Call<RegisterResponse>,
+                                    response: Response<RegisterResponse>
+                                ) {
+
+                                    if (response.isSuccessful && response.body() != null) {
+                                        Desk360Config.instance.getDesk360Preferences()?.data =
+                                            response.body()!!.data
+                                        Desk360Config.instance.getDesk360Preferences()?.meta =
+                                            response.body()!!.meta
+
+                                        cloneRequest?.enqueue(this@BaseCallback)
+
+                                    }
+                                }
+
+                            })
+
+                    }
+                    return
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+
+                }
+            }
+
+        } else {
+            onResponseSuccess(call, response)
+
+        }
     }
 
     abstract fun onResponseSuccess(call: Call<T>, response: Response<T>)
 
     override fun onFailure(call: Call<T>, t: Throwable) {
         t.message?.let { Log.e("", it) }
+        listener?.let {
+            listener!!.connectionError("", 123)
+        }
+    }
+
+    private fun retry() {
+        cloneRequest?.clone()?.enqueue(this)
     }
 }
