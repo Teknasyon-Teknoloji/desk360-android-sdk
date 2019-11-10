205bcb9e84ee0f45b7c62d2e9413d0e8416ca330
Sabir Kahraman <sabirkahraman@teknasyon.com>
2019-11-11 00:32:25 +0300

    [fix] fixed style inputlayouts

--- desk360/src/main/java/com/teknasyon/desk360/themev2/Desk360InputLayout.kt
+++ desk360/src/main/java/com/teknasyon/desk360/themev2/Desk360InputLayout.kt
@@ -3,71 +3,117 @@ package com.teknasyon.desk360.themev2
 import android.content.Context
 import android.os.Build
 import android.util.AttributeSet
+import androidx.appcompat.view.ContextThemeWrapper
 import com.google.android.material.textfield.TextInputLayout
 import com.teknasyon.desk360.R
-import com.teknasyon.desk360.helper.Desk360Constants
+import com.teknasyon.desk360.helper.Desk360Constants.currentTheme
 
 class Desk360InputLayout : TextInputLayout {
 
     init {
 
-        when (Desk360Constants.currentTheme) {
+        when (currentTheme) {
             1 -> {
                 this.setBoxBackgroundMode(BOX_BACKGROUND_NONE)
-                context.setTheme(R.style.input_type1)
-                invalidate()
             }
             2 -> {
                 this.setBoxBackgroundMode(BOX_BACKGROUND_OUTLINE)
-                context.setTheme(R.style.input_type1)
-                invalidate()
             }
             3 -> {
                 this.setBoxBackgroundMode(BOX_BACKGROUND_OUTLINE)
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    this.background = resources.getDrawable(R.drawable.type_3_bg_with_shadow,null)
-                }
-                else
+                    this.background = resources.getDrawable(R.drawable.type_3_bg_with_shadow, null)
+                } else {
                     this.background = resources.getDrawable(R.drawable.type_3_bg_with_shadow)
-
-                context.setTheme(R.style.input_type1)
-                invalidate()
+                }
             }
             4 -> {
                 this.setBoxBackgroundMode(BOX_BACKGROUND_NONE)
-                context.setTheme(R.style.input_type1)
-                invalidate()
             }
             5 -> {
                 this.setBoxBackgroundMode(BOX_BACKGROUND_OUTLINE)
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    this.background = resources.getDrawable(R.drawable.type_3_bg_with_shadow,null)
-                }
-
-                else
+                    this.background = resources.getDrawable(R.drawable.type_3_bg_with_shadow, null)
+                } else {
                     this.background = resources.getDrawable(R.drawable.type_3_bg_with_shadow)
-
-                context.setTheme(R.style.input_type1)
-                invalidate()
+                }
             }
             else -> {
                 this.setBoxBackgroundMode(BOX_BACKGROUND_NONE)
-                context.setTheme(R.style.input_type1)
-                invalidate()
             }
         }
     }
 
+    constructor(context: Context) : super(
+        when (currentTheme) {
+            1 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            2 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            3 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            4 -> {
+                ContextThemeWrapper(context, R.style.input_type4)
+            }
+            5 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            else -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
 
-    constructor(context: Context) : super(context)
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
-
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
-        context,
-        attrs,
-        defStyle
+        }
     )
 
+    constructor(context: Context, attrs: AttributeSet) : super(
+        when (currentTheme) {
+            1 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            2 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            3 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            4 -> {
+                ContextThemeWrapper(context, R.style.input_type4)
+            }
+            5 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            else -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+
+        }, attrs
+    )
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        when (currentTheme) {
+            1 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            2 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            3 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            4 -> {
+                ContextThemeWrapper(context, R.style.input_type4)
+            }
+            5 -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+            else -> {
+                ContextThemeWrapper(context, R.style.input_type1)
+            }
+
+        }, attrs, defStyle
+    )
 }
 
