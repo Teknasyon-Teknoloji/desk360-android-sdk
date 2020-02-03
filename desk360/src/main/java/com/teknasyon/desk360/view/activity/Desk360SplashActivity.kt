fc66bdd988cf940c56a59971984b70dcafc5f653
Emre Alp <emre.alp@ozu.edu.tr>
2020-02-03 12:12:24 +0300

    beta 0.6.7

--- desk360/src/main/java/com/teknasyon/desk360/view/activity/Desk360SplashActivity.kt
+++ desk360/src/main/java/com/teknasyon/desk360/view/activity/Desk360SplashActivity.kt
@@ -1,8 +1,6 @@
 package com.teknasyon.desk360.view.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.teknasyon.desk360.R
 import com.teknasyon.desk360.viewmodel.GetTypesViewModel
@@ -12,8 +10,9 @@ class Desk360SplashActivity : AppCompatActivity() {
     var notificationToken: String? = null
     var targetId: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_desk_360)
 
         val bundle = intent.extras
