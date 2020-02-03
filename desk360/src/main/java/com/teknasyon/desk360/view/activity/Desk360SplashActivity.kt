1d3b02ebe3aa86a7780ba7313ebf155c34d263b8
Emre Alp <emre.alp@ozu.edu.tr>
2020-02-03 13:44:24 +0300

    beta 0.6.7

--- desk360/src/main/java/com/teknasyon/desk360/view/activity/Desk360SplashActivity.kt
+++ desk360/src/main/java/com/teknasyon/desk360/view/activity/Desk360SplashActivity.kt
@@ -3,12 +3,14 @@ package com.teknasyon.desk360.view.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.teknasyon.desk360.R
+import com.teknasyon.desk360.helper.Desk360Config
+import com.teknasyon.desk360.helper.Desk360Constants
 import com.teknasyon.desk360.viewmodel.GetTypesViewModel
 
 class Desk360SplashActivity : AppCompatActivity() {
 
-    var notificationToken: String? = null
-    var targetId: String? = null
+    private var notificationToken: String? = null
+    private var targetId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -17,8 +19,19 @@ class Desk360SplashActivity : AppCompatActivity() {
 
         val bundle = intent.extras
         bundle?.let {
+
             notificationToken = bundle.getString("token")
             targetId = bundle.getString("targetId")
+
+            Desk360Config().context = this
+            Desk360Constants.currentTheme = 1
+
+            Desk360Constants.desk360Config(
+                app_key = bundle.getString("app_key")!!,
+                app_version = bundle.getString("app_version")!!,
+                baseURL = bundle.getString("baseURL"),
+                device_token = bundle.getString("device_token")
+            )
         }
 
         val getTypesViewModel = GetTypesViewModel(this, notificationToken, targetId)
